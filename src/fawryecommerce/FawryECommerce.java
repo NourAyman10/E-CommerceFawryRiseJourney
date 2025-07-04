1d6b0904278bfe75ec62ed158ef1001f99fb69a8
Nourhan Ayman <83882344+Nour10102002@users.noreply.github.com>
2025-07-04 07:30:04 +0200

    initialize project and generate product class

---
+++ src/fawryecommerce/FawryECommerce.java
@@ -0,0 +1,9 @@
+package fawryecommerce;
+public class FawryECommerce
+{
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
---
+++ src/fawryecommerce/Product.java
@@ -0,0 +1,11 @@
+package fawryecommerce;
+public class Product
+{
+    private String Name;
+    private String Price;
+    private String Quantity;
+    private Boolean IsExpire;
+    private Boolean IsShipping;
+    private Double Weight;
+    
+}
